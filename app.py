0cee5d9bb008311dcd4e8b03a002348cc5186592
Jinfay Justin Yuan <jyuan@olin.edu>
2019-11-12 15:08:45 -0500

    add site navigation and basic table

--- app.py
+++ app.py
@@ -3,8 +3,17 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
+
+@app.route('/course_dir/')
+def course_dir():
+    return render_template('course_dir.html')
+
+@app.route('/roadmap/')
+def roadmap():
+    return render_template('roadmap.html')
+
 
 if __name__ == '__main__':
     app.run()
