4da8cfaa0efe973e6ef702ae63da45e8c8d2c4ec
jamesho <8.james.ho@gmail.com>
2019-11-19 17:27:28 -0500

    add getDatetime function

--- courseProcessing.py
+++ courseProcessing.py
@@ -22,49 +22,84 @@ def getAllCourses():
 #%%
 def getCourseTimes(text):
     '''
-    Looks for the start and end times of a course
+    Parses the description from the 'Faculty / Schedule' column of the csv from my.olin.edu add/drop
+    and creates a dataframe with the correct columns
+    text: String, the text you want to parse. 
     '''
     # Import the table
     cwd = Path.cwd()
     filePath = Path(cwd / 'courseData/2019_S1_offering.csv')
     courseTable = pd.read_csv(filePath)
-
-    # while("" in instructors): 
-    #     instructors.remove("")
+    # Split each line into an array
     eachLine = text.splitlines()
     print(eachLine)
+    # Iterate through each array and use RegEx to pick out the parts you need
     for line in eachLine:
+        
         # Parse the text to get usefull information
         timeRegexPat = "([a-zA-Z]+)\s(\d{2}):(\d{2})-(\d{2}):(\d{2})([A-Z]{2})"
         instructorRegexPat = "(.*?)(?=\/)"
 
         instructorRegexPatAM = "(.*?) / ([a-zA-Z]+) (\d{2}:\d{2}[a-zA-Z]+)-(\d{2}:\d{2}[a-zA-Z]+); (.*)"
         instructorRegexPatPM = "(.*?) / ([a-zA-Z]+) (\d{2}:\d{2})-(\d{2}:\d{2}[a-zA-Z]+); (.*)"
+        instructorRegexPatNoDays = "(.*?) / (\d{2}:\d{2})-(\d{2}:\d{2}[a-zA-Z]+); (.*)"
         matchedObject = re.match(instructorRegexPatAM, line)
         
         # This check here is intended to switch between AM and PM time patterns
         if matchedObject is None:
             matchedObject = re.match(instructorRegexPatPM, line)
-        
-        instructor = matchedObject.group(1)
-        day = matchedObject.group(2)
-        startTime = matchedObject.group(3)
-        endTime = matchedObject.group(4)
-        location = matchedObject.group(5)
+
+        # handles weird case where there is a 00:00 time and no start date
+        if "00:00-00:00AM;" in line:
+            matchedObject = re.match(instructorRegexPatNoDays, line)
+            instructor = matchedObject.group(1)
+            day = None
+            startTime = "01:00AM"
+            endTime = "01:00AM"
+            location = matchedObject.group(4)
+
+        # Handles the normal cases
+        else:
+            instructor = matchedObject.group(1)
+            day = matchedObject.group(2)
+            startTime = matchedObject.group(3)
+            endTime = matchedObject.group(4)
+            location = matchedObject.group(5)
+
+        # Add PM to the end of the startTime string so the format is the same for all time strings
+        if "PM" in endTime and "AM" not in startTime:
+            startTime = startTime + "PM"
+
+        # Convert all the strings into the right data types
+        startTime = getDateTime(startTime)
+        endTime = getDateTime(endTime)
     
         print(instructor, day, startTime, endTime, location)
+        print("\n")
+    # return instructor, day, startTime, endTime, location
     pass
 #%%
 
 descriptions = courseTable['Faculty / Schedule'].values
-for i in range(3):
+for i in range(10):
     getCourseTimes(descriptions[i])
+    print(i)
     
 #%%
 def getDateTime(dateString):
+    '''
+    This function takes in the string that represents a time and returns a datetime object
+    dateString: String, represents the time 00:00AM as an example
+    '''
     dateObject = datetime.strptime(dateString, "%I:%M%p")
-    print(dateObject)
-    return dateObject
+    # print(dateObject.time())
+    return dateObject.time()
+#%%
+def getWeekdays(days):
+    '''
+    Given a string of characters, converts it into a boolean tuple with length of 7, 
+    each representing whether or not the course is offered on each day
+    '''
 
  
 # TODO:
